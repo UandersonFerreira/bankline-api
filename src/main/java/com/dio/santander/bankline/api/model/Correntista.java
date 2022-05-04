78fe0e8c3de25c03ed6de99f2a7b66503a3890e2
UandersonFerreira <uanderson.oliveira@estudante.ifto.edu.br>
2022-05-03 21:26:11 -0300

    mapeando as entidade com o JPA

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,10 +1,26 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.*;
+
+@Entity//define a classe como uma entidade
+@Table(name = "tab_corresntista")
 public class Correntista {
+   @Id//informa quem será a key primary da entidade/tabela
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   //indica que será criado um id automaticamente é que serão unicos.
    private Integer id;
+   @Column(length = 20)//define o tamanho da coluna
    private String cpf;
-   private String nome;
-   private Conta conta;
+    @Column(length = 60)//define o tamanho da coluna
+    private String nome;
+   @Embedded
+    private Conta conta;
+   /*
+    @Embedded -> as informações que estiverem nesta classe estarão na mesma
+   //tabela que a tabela principal, a tabela que possui uma referência a esta classe
+   (Uma forma de relação entre classes entre aspas)
+
+    */
 
 
     public Conta getConta() {
--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -1,12 +1,20 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "data_hora")
     private LocalDateTime dataHora;
     private String descricao;
+    //quando o nome da coluna no banco e igual ao atributo não a necessidade de definir um nome para a coluna com o @Column, pois o JPA irá entender
     private Double valor;
+    @Enumerated(EnumType.STRING)//salvo o proprio valor literal/real(Receita ou Despesa)
     private MovimentacaoTipo tipoMovimentacao;
 
     public Integer getId() {
